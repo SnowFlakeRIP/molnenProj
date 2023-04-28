0f7db58e100e2646b648b29b36b86b6b30c91f66
SnowFlakeRIP <schmakov8@gmail.com>
2023-04-28 16:28:51 +0500

    123

---
+++ src/components/UI/CheckTask.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <slot></slot>
+    
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style
+    lang="scss"
+    scoped
+>
+.task-container{
+
+    &:hover{
+        .delete-task{
+            display: block;
+            margin-left: auto;
+            
+            cursor: pointer;
+            background: none;
+            border: none;
+        }
+    }
+    
+    .delete-task{
+        display: none;
+    }
+}
+</style>
